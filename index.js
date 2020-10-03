f6fe72aea8f76fa2998d8228d8f489f879d086df
rafsan <rafsanamin18@gmail.com>
2020-10-03 19:07:15 +0600

    50.4

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mypty.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = 5000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+client.connect((err) => {
+    const productsCollection = client.db("emaJohnStore").collection("products");
+
+    app.post("/addProduct", (req, res) => {
+        const products = req.body;
+        productsCollection.insertMany(products).then((result) => {
+            res.send(result.insertedCount);
+            console.log(result.insertedCount);
+        });
+    });
+
+    app.get("/products", (req, res) => {
+        productsCollection
+            .find({})
+            .limit(20) // limit 20 deleted
+            .toArray((err, documents) => {
+                res.send(documents);
+            });
+    });
+
+    app.get("/product/:key", (req, res) => {
+        productsCollection
+            .find({ key: req.params.key })
+            .toArray((err, documents) => {
+                res.send(documents[0]);
+            });
+    });
+});
+
+app.listen(port);
